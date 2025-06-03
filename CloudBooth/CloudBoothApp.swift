820fd9e69cedc0bae5aff908036b5ecf3c3021d4
QuackityDuck <navaneethnandakumar12@gmail.com>
2025-06-03 19:52:19 +0530

    Initial Commit

---
+++ CloudBooth/CloudBoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudBoothApp.swift
+//  CloudBooth
+//
+//  Created by Navaneeth on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CloudBoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloudBoothTests/CloudBoothTests.swift
@@ -0,0 +1,17 @@
+//
+//  CloudBoothTests.swift
+//  CloudBoothTests
+//
+//  Created by Navaneeth on 6/3/25.
+//
+
+import Testing
+@testable import CloudBooth
+
+struct CloudBoothTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
